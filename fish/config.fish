999c8f5aa4a5c8bd812f72eecc59a5684f18d8b7
Alex Varakin <avarakin@gmail.com>
2026-04-15 18:02:21 -0400

    Update fish config and hyprland startup services
    - Add SHELL variable export for fish in fish config
    - Add gsu alias for polkit-wrapped commands in fish config
    - Enable dropbox autostart in hyprland
    - Add polkit-kde-authentication-agent to hyprland exec-once services

--- fish/config.fish
+++ fish/config.fish
@@ -5,3 +5,5 @@ export PATH="$HOME/.local/bin:$HOME/dot/scripts:$PATH"
 #abbr obs "GLIBC_TUNABLES=glibc.rtld.execstack=2 obs"
 alias webcam "sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2 ; gphoto2 --stdout --set-config liveviewsize=0 --capture-movie | ffmpeg  -i - -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2  -r 30 /dev/video0"
 alias resolve "__NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia /opt/resolve/bin/resolve"
+export SHELL=/usr/bin/fish
+alias gsu 'pkexec env WAYLAND_DISPLAY="$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY" XDG_RUNTIME_DIR=/run/user/0'
